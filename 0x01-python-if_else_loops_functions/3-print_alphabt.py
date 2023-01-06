15c30022e6b9dfb423e1d7db3e3c7558a32c9a6a
Kibuye24 <joshkibbz@gmail.com>
2023-01-05 16:21:20 -0800

    Print alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for ch in range(97, 123):
-        if ch != 101 and ch != 113:
-                print("{:c}".format(ch), end='')
+for xup in range(ord('a'), ord('z') +1:
+        if chr(xup) != 'q' and chr(xup) != 'e':
+        print("{}".format(chr(xup)), end='')
